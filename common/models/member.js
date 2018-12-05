3782f9dbebd16f515e78bbc4b6f810366b5a9928
Robert Barnett <therobertbarnett@gmail.com>
2018-12-04 17:08:27 -0800

    Maps members tags to member card #4

--- common/models/member.js
+++ common/models/member.js
@@ -15,39 +15,4 @@ module.exports = function (Member) {
     }
     }
   });
-    
-  // Member.getRoster = function (id, cb) {
-  //   const { Tag } = Member.app.models;
-  //   Member.find({
-  //     where: {
-  //       isHidden: false
-  //     }
-  //   })
-  //   .then((results) => {
-  //     return Promise.all(results.map((result) => {
-  //       return Player.findById(result.playerId)
-  //         .then(player => player)
-  //         .catch(error => console.log(error));
-  //     }));
-  //   })
-  //   .then((allResults) => cb(null, allResults.filter(x => !!x)))
-  //   .catch(err => console.log(err));
-  // };
-
-  // Member.remoteMethod('getRoster', {
-  //   description: 'Returns required data from all members to populate member cards on roster.',
-  //   accepts: {
-  //     arg: 'id',
-  //     type: 'number'
-  //   },
-  //   http: {
-  //     path: '/getPlayers',
-  //     verb: 'get'
-  //   },
-  //   returns: {
-  //     arg: 'data',
-  //     type: 'object',
-  //     root: true
-  //   },
-  // });
 };
